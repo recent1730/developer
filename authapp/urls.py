a22350678d70075ae7b97549e7d67265a02e01a0
Rohit Patel <recent3088@gmail.com>
2021-04-30 13:36:20 +0530

    done

--- authapp/urls.py
+++ authapp/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView,LogoutView
 
 app_name='authapp'
 urlpatterns=[
-    path('register/', register, name='register'),
-    path('', LoginView.as_view(template_name='authapp/login.html'), name='login'),
+    path('', register, name='register'),
+    path('login/', LoginView.as_view(template_name='authapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='authapp/logout.html'), name='login'),
 ]
